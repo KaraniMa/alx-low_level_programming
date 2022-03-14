6f84818ef06116eb377381b04c50ef7d01fd3e81
KaraniMa <99687854+KaraniMa@users.noreply.github.com>
2022-03-14 23:31:11 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - prints out the message
  *
  * return: Always 0 if compiled without errors
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
