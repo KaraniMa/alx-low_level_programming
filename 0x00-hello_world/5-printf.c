a7fd3dc7c7188ab4fb5d85d0e87b2e22047e22ef
KaraniMa <karanimaggy2@gmail.com>
2022-03-14 13:19:50 -0700

    Using printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the message
+ *
+ * return: Always 0 if compiled without errors
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
